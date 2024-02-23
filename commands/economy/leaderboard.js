28e63971a743eb0ded2ca5427f0546cb57a621cf
Ringo Origo <59732670+ringothefly@users.noreply.github.com>
2024-02-23 11:13:34 -0500

    Global economy leaderboard command

---
+++ commands/economy/leaderboard.js
@@ -0,0 +1,49 @@
+/*
+    Display the ten users with the highest currency balances
+        Allow the user to choose whether the leaderboard is global or local (per guild)
+*/
+
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { botName, currencyName } = require('../../config.json');
+const UserProfile = require('../../schemas/UserProfile.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('leaderboard')
+        .setDescription(`View a leaderboard of the users with the top ${currencyName} balances.`)
+        .addBooleanOption(option =>
+            option.setName('ephemeral')
+                .setDescription('Decide whether the command\'s response is ephemeral or not. (Only you can see ephemeral messages')
+                .setRequired(true)),
+
+    async execute(interaction) {
+        const ephemeral = interaction.options.getBoolean('ephemeral');
+        const userList = await UserProfile.find();
+
+        // Defer the reply as this can take a while
+        await interaction.deferReply({ ephemeral: ephemeral });
+
+        // Sort all users by their balances. Descending order.
+        userList.sort((a, b) => {
+            return b.balance - a.balance;
+        }).slice(0, 10);
+
+        const embed = new EmbedBuilder()
+            .setColor('f0b3be')
+            .setAuthor({
+                name: `${botName}`,
+                iconURL: 'https://cdn.discordapp.com/avatars/995022636549681152/2617cb7afb19882f89aa5ee1bec1c86a',
+            })
+            .setTitle(`Global ${currencyName} Leaderboard:`)
+            .setDescription(userList.map((profile, index) => {
+                return `**${index + 1}.** <@${profile.userID}>: ${profile.balance}`;
+            }).join('\n'))
+            .setTimestamp()
+            .setFooter({
+                text: `Brought to you by ${botName}`,
+                iconURL: 'https://cdn.discordapp.com/avatars/995022636549681152/2617cb7afb19882f89aa5ee1bec1c86a',
+            });
+
+        await interaction.followUp({ embeds: [embed], ephemeral: ephemeral });
+    },
+};
