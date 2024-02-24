d40ce620333f162254bf34d742263eaaa664d96e
Ringo Origo <59732670+ringothefly@users.noreply.github.com>
2024-02-24 09:12:09 -0500

    Changed to stats command to show more than balance

--- commands/economy/stats.js
+++ commands/economy/stats.js
@@ -1,6 +1,9 @@
 /*
-    Display the the balance of the specified user
-        If the user doesn't exist in the database, then create their profile and set their balance to zero.
+    Display the the in-depth breakdown of the specified user's haul statistics
+        If the user doesn't exist in the database, then create their profile and set everything to its default value.
+            In this event, do not display any stats, as the user does not have any.
+        If the user DOES exist, display their whole statistical breakdown
+            This includes their balance, total number of hauls, and the percentage breakdown of each haul found.
 */
 
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
@@ -9,7 +12,7 @@ const UserProfile = require('../../schemas/UserProfile.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('balance')
+        .setName('stats')
         .setDescription(`View the user's specified balance of${currencyName}s.`)
         .addUserOption(option =>
             option.setName('target')
@@ -32,9 +35,11 @@ module.exports = {
                 userProfile = new UserProfile({
                     userID: target.id,
                 });
+                return await interaction.reply({ content: 'You\'ve never found any hauls, so you have no stats! Use `/haul` to put yourself on the board.', ephemeral: ephemeral });
             }
 
             const balance = userProfile.balance;
+            const hauls = userProfile.totalHauls;
             const embed = new EmbedBuilder()
                 .setColor('f0b3be')
                 .setAuthor({
@@ -48,7 +53,10 @@ module.exports = {
                 })
                 .setTitle(`${target.displayName}'s Haul Stats`)
                 .addFields(
-                    { name: 'Current Balance: ', value: `${balance}` },
+                    { name: '__Current Balance__:', value: `${balance} ${currencyName}s` },
+                    // I hate having this extremely long line, but the formatting is awful if this is split into multiple lines, so unfortunately I am keeping it.
+                    { name: '__Numeric Haul Stats__:', value: `Total Hauls Found: **${hauls}**\nLegendary Hauls Found: **${userProfile.legendaryHauls}**\nEpic Hauls Found: **${userProfile.epicHauls}**\nUncommon Hauls Found: **${userProfile.uncommonHauls}**\nCommon Hauls Found: **${userProfile.commonHauls}**\nHaul Failures: **${userProfile.haulFailures}**`, inline: true },
+                    { name: '__Haul-Type Percentages__:', value: `Legendary Hauls: **${(userProfile.legendaryHauls / hauls * 100).toFixed(1)}%**\nEpic Hauls: **${(userProfile.epicHauls / hauls * 100).toFixed(1)}%**\nUncommon Hauls: **${(userProfile.uncommonHauls / hauls * 100).toFixed(1)}%**\nCommon Hauls: **${(userProfile.commonHauls / hauls * 100).toFixed(1)}%**\nHaul Failures: **${(userProfile.haulFailures / hauls * 100).toFixed(1)}%**\n`, inline: true },
                 );
 
             await interaction.reply({ embeds: [embed], ephemeral: ephemeral });
