cba9fd1664c49f4d15e27b04c2f457e334079b64
Ringo Origo <59732670+ringothefly@users.noreply.github.com>
2024-02-29 20:16:18 -0500

    Users can now pay currency to other users

---
+++ commands/economy/pay.js
@@ -0,0 +1,90 @@
+/*
+        Allow a user to pay another user, removing the designated amount from their balance and adding it to the other user's
+*/
+
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { currencyName, botName } = require('../../config.json');
+const UserProfile = require('../../utils/schemas/UserProfile.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('pay')
+        .setDescription(`Gift some ${currencyName} to the designated user`)
+        .addUserOption(option =>
+            option.setName('target')
+                .setDescription('The user to pay')
+                .setRequired(true))
+        .addIntegerOption(option =>
+            option.setName('quantity')
+                    .setDescription(`The amount of ${currencyName} to pay the designated user`)
+                    .setRequired(true)),
+    async execute(interaction) {
+        const target = interaction.options.getUser('target');
+        const payment = interaction.options.getInteger('quantity');
+
+        let loaner;
+        let recipient;
+
+        if (target.id == interaction.user.id) {
+            return await interaction.reply({ content: 'You cannot pay yourself.', ephemeral: true });
+        }
+
+        try {
+            // Find the loaner and recipients entries in the database.
+            loaner = await UserProfile.findOne({
+                userID: interaction.user.id,
+            });
+
+            recipient = await UserProfile.findOne({
+                userID: target.id,
+            });
+
+            // Create entires for the recipient and loaner if they do not already exist.
+            if (!loaner) {
+                loaner = new UserProfile({
+                    userID: interaction.user.id,
+                });
+            }
+            if (!recipient) {
+                recipient = new UserProfile({
+                    userID: target.id,
+                });
+            }
+
+            // If the loaner does not have a high enough balance, do not allow them to pay
+            if (loaner.balance < payment) {
+                return await interaction.reply({ content: `You cannot pay ${target.displayName} ${payment} ${currencyName}s, as your current balance is ${loaner.balance}!`, ephemeral: true });
+            }
+
+            // remove the payment amount from the loader's acconut, and add it to the recipient's account
+            recipient.balance += payment;
+            loaner.balance -= payment;
+
+            const embed = new EmbedBuilder()
+                .setColor('f0b3be')
+                .setAuthor({
+                    name: `${botName}`,
+                    iconURL: 'https://cdn.discordapp.com/avatars/995022636549681152/2617cb7afb19882f89aa5ee1bec1c86a',
+                })
+                .setTitle('Successful Payment!')
+                .addFields(
+                    { name: `${target.displayName}'s Balance:`, value: `${recipient.balance} ${currencyName}s` },
+                    { name: `${interaction.user.displayName}'s Balance:`, value: `${loaner.balance} ${currencyName}s` },
+                )
+                .setTimestamp()
+                .setFooter({
+                    text: `Brought to you by ${botName}`,
+                    iconURL: 'https://cdn.discordapp.com/avatars/995022636549681152/2617cb7afb19882f89aa5ee1bec1c86a',
+                });
+
+            // Sync the data with the database
+            await recipient.save();
+            await loaner.save();
+
+            await interaction.reply({ embeds: [embed] });
+        }
+        catch (e) {
+            console.log(e);
+        }
+    },
+};
